26ac3d3c7a8a319be12beb3b305ce70d2fa9aee0
GilbyScarChest <tylerdavidgilbert@gmail.com>
2019-05-26 18:19:30 -0700

    edited Populate_Table.js. Made more generic variables.

--- JavaScript/populate_table.js
+++ JavaScript/populate_table.js
@@ -1,32 +1,35 @@
+//Get the data from data.js
+let tableData = data;
+
 // Get a reference to the table body
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
 console.log(data);
 
 // // Loop Through `data` and console.log each weather report object
-data.forEach(function(weatherReport) {
-  console.log(weatherReport);
+tableData.forEach(function(item) {
+  console.log(item);
 });
 
 // // Use d3 to update each cell's text with
 // // weather report values (weekday, date, high, low)
-data.forEach(function(weatherReport) {
-  console.log(weatherReport);
-  var row = tbody.append("tr");
-  Object.entries(weatherReport).forEach(function([key, value]) {
+tableData.forEach(function(item) {
+  console.log(item);
+  let row = tbody.append("tr");
+  Object.entries(item).forEach(function([key, value]) {
     console.log(key, value);
     // Append a cell to the row for each value
     // in the weather report object
-    var cell = row.append("td");
+    let cell = row.append("td");
     cell.text(value);
   });
 });
 
 // BONUS: Refactor to use Arrow Functions!
-// data.forEach((weatherReport) => {
+// data.forEach((item) => {
 //   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(([key, value]) => {
+//   Object.entries(item).forEach(([key, value]) => {
 //     var cell = row.append("td");
 //     cell.text(value);
 //   });
